ca13bf5ed011c6150d0b5dca0db8cebb15eb0087
Cornel-PC <corneloaie@gmail.com>
2018-04-25 18:35:27 +0300

    final2

--- src/DataImport.java
+++ src/DataImport.java
@@ -1,11 +1,7 @@
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 
-import java.io.IOException;
-
 public class DataImport {
-
-    
 
 
     public static Instances getInstancesFromARFF(String filePath) throws Exception {
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
-import weka.classifiers.meta.AdditiveRegression;
-import weka.classifiers.meta.AttributeSelectedClassifier;
 import weka.classifiers.trees.J48;
-import weka.classifiers.trees.M5P;
-import weka.classifiers.trees.REPTree;
-import weka.classifiers.trees.RandomForest;
 import weka.core.Instances;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.Remove;
 import weka.gui.beans.ModelPerformanceChart;
 import weka.gui.beans.ThresholdDataEvent;
+import weka.gui.treevisualizer.PlaceNode2;
+import weka.gui.treevisualizer.TreeVisualizer;
 import weka.gui.visualize.PlotData2D;
 
-import java.util.Date;
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Main {
@@ -22,13 +25,38 @@ public class Main {
 
             Instances train = DataImport.getInstancesFromARFF("C:/Users/Cornel-PC/Desktop/occupancy_data/datatraining - Copy.arff");
             Instances test = DataImport.getInstancesFromARFF("C:/Users/Cornel-PC/Desktop/occupancy_data/datatest - Copy.arff");
-            train.setClassIndex(train.numAttributes() - 1);
+            BufferedReader attrfile = new BufferedReader(new FileReader("C:/Users/Cornel-PC/Desktop/occupancy_data/attributes.txt"));
+            List<Integer> myList = new ArrayList<>();
+            String line;
+
+            while ((line = attrfile.readLine()) != null) {
+                for (int n = 0; n < train.numAttributes(); n++) {
+                    if (train.attribute(n).name().equalsIgnoreCase(line)) {
+                        if (!myList.contains(n))
+                            myList.add(n);
+                    }
+                }
+            }
+
+            int[] attrs = myList.stream().mapToInt(i -> i).toArray();
+            Remove remove = new Remove();
+            remove.setAttributeIndicesArray(attrs);
+            remove.setInvertSelection(false);
+            remove.setInputFormat(train); // init filter
+
+            Instances filtered = Filter.useFilter(train, remove);
+
+
+            filtered.setClassIndex(filtered.numAttributes() - 1);
             test.setClassIndex(test.numAttributes() -1);
             Classifier cls = new J48();
-            cls.buildClassifier(train);
-            Evaluation evaluation = new Evaluation(train);
+            cls.buildClassifier(filtered);
+            Evaluation evaluation = new Evaluation(filtered);
             evaluation.evaluateModel(cls, test);
             System.out.println(evaluation.toSummaryString("\nResults\n======\n", false));
+
+
+            // display graphs
             final javax.swing.JFrame jf = new javax.swing.JFrame();
             jf.getContentPane().setLayout(new java.awt.BorderLayout());
             final ModelPerformanceChart as = new ModelPerformanceChart();
@@ -47,6 +75,25 @@ public class Main {
             });
             jf.setSize(800, 600);
             jf.setVisible(true);
+
+
+            // display classifier
+            final javax.swing.JFrame jFrame =
+                    new javax.swing.JFrame("Weka Classifier Tree Visualizer: J48");
+            jFrame.setSize(4000, 4000);
+            jFrame.getContentPane().setLayout(new BorderLayout());
+            TreeVisualizer tv = new TreeVisualizer(null, ((J48) cls).graph(),
+                    new PlaceNode2());
+            jFrame.getContentPane().add(tv, BorderLayout.CENTER);
+            jFrame.addWindowListener(new java.awt.event.WindowAdapter() {
+                public void windowClosing(java.awt.event.WindowEvent e) {
+                    jFrame.dispose();
+                }
+            });
+
+            jFrame.setVisible(true);
+            tv.fitToScreen();
+
         } catch (Exception ex) {
             ex.printStackTrace();
             System.err.println(ex.getMessage());
